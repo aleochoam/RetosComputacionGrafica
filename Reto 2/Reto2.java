25600e6a6836ed9b5f44c6369389039e3f6d9a95
Alejandro <alejo8a_2@hotmail.com>
2016-02-09 18:24:42 -0500

    Pasando las cosas a git

---
+++ Reto 2/Reto2.java
@@ -0,0 +1,155 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+import java.util.Random;
+
+public class Reto2 extends JPanel{
+    private static final int INSIDE = 0;
+    private static final int LEFT = 1;
+    private static final int RIGHT = 2;
+    private static final int BOTTOM = 4;
+    private static final int TOP = 8;
+
+    private static  int yMax = 125;
+    private static  int yMin = -125;
+    private static  int xMax = -125;
+    private static  int xMin = 125;
+
+    private int h, w;
+    private Graphics2D g2d;
+
+    @Override
+    public void paintComponent(Graphics g) {
+       super.paintComponent(g);
+
+       g2d = (Graphics2D) g;
+
+       g2d.setColor(Color.BLUE);
+
+       // size es el tamaño de la ventana.
+       Dimension size = getSize();
+       // Insets son los bordes y los títulos de la ventana.
+       Insets insets = getInsets();
+       w =  size.width - insets.left - insets.right;
+       h =  size.height - insets.top - insets.bottom;
+       xMax = xMax + w/2;
+       yMax = yMax + h/2;
+       xMin = xMin - w/2;
+       yMin = yMin - h/2;
+       drawLines();
+
+    }
+
+    public void drawLines(){
+        Random r = new Random();
+        for (int i = 0; i<10000; i++) {
+/*
+            int x0 = Math.abs(r.nextInt())%10 * w -w/2;
+            int y0 = Math.abs(r.nextInt())%10 * h -h/2;
+
+            int x1 = Math.abs(r.nextInt())%10 * w -w/2;
+            int y1 = Math.abs(r.nextInt())%10 * h -h/2;
+*/
+            int xp0 = (int) (Math.random() * w);
+            int yp0 = (int) (Math.random() * h);
+
+            int xp1 = (int) (Math.random() * w);
+            int yp1 = (int) (Math.random() * h);
+
+            g2d.drawLine(xp0, yp0, xp1, yp1);
+            CHAlgol(xp0, yp0, xp1, yp1);
+        }
+    }
+
+    /*
+     *Tomado de https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
+     */
+
+    public int computeOutCode(double x, double y){
+        int code = INSIDE;
+        if (x < xMin)
+            code |= LEFT;
+        else if (x > xMax)
+            code |= RIGHT;
+        if (y < yMin)
+            code |= BOTTOM;
+        else if (y > yMax)
+            code |= TOP;
+
+        return code;
+    }
+
+    public void CHAlgol(int x0, int y0, int x1, int y1){
+        int outCode0 = computeOutCode(x0, y0);
+        int outCode1 = computeOutCode(x1, y1);
+        boolean accept = false;
+
+        while(true){
+            if((outCode0 | outCode1) == 0){
+                accept = true;
+                break;
+            }else if ((outCode0 & outCode1) != 0 ) {
+                break;
+            }else{
+                int x = -1, y = -1;
+                int outCodeOut = outCode0 != 0 ? outCode0 : outCode1;
+
+                if ((outCodeOut & TOP) != 0) {
+                    x = x0 + (x1-x0) * (yMax-y0) / (y1-y0);
+                    y = yMax;
+                } else if ((outCodeOut & BOTTOM) != 0) { // point is below the clip rectangle
+                    x = x0 + (x1 - x0) * (yMin - y0) / (y1 - y0);
+                    y = yMin;
+                } else if ((outCodeOut & RIGHT) != 0) {  // point is to the right of clip rectangle
+                    y = y0 + (y1 - y0) * (xMax - x0) / (x1 - x0);
+                    x = xMax;
+                } else if ((outCodeOut & LEFT) != 0) {   // point is to the left of clip rectangle
+                    y = y0 + (y1 - y0) * (xMin - x0) / (x1 - x0);
+                    x = xMin;
+                }
+
+                if (outCodeOut == outCode0) {
+                    x0 = x;
+                    y0 = y;
+                    outCode0 = computeOutCode(x0, y0);
+                } else {
+                    x1 = x;
+                    y1 = y;
+                    outCode1 = computeOutCode (x1, y1);
+                }
+            }
+        }
+
+        if(accept){
+           //DrawRectangle(xMin,yMin,xMax,ymMax);
+            g2d.setColor(Color.RED);
+
+            g2d.drawLine(x0,y0,x1,y1);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        // Crear un nuevo Frame
+        JFrame frame = new JFrame("Clipping");
+        // Al cerrar el frame, termina la ejecución de este programa
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Agregar un JPanel que se llama Points (esta clase)
+        frame.add(new Reto2());
+        // Asignarle tamaño
+        frame.setSize(500, 500);
+        // Poner el frame en el centro de la pantalla
+        frame.setLocationRelativeTo(null);
+        // Mostrar el frame
+        frame.setVisible(true);
+    }
+
+
+}
