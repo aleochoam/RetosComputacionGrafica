8a5980c4ea60dddf51af4f07c4fcf6e468f44556
Felipe <fmontoyalvarez@gmail.com>
2016-02-15 18:59:36 -0500

    Multiplicacion de matrices

--- Reto 3/Maths/Matriz2D.java
+++ Reto 3/Maths/Matriz2D.java
@@ -6,4 +6,30 @@ public class Matriz2D {
     public double[][] getMatriz(){
         return matriz;
     }
+
+    public static double[][] multiplicar(double[][] m1, double[][] m2 ){
+        int fil1 = m1.length;
+        int col1 = m1[0].length;
+        int fil2 = m2.length;
+        int col2 = m2[0].length;
+        double [][] result = new double[fil1][col2];
+        if(fil1 != col2){
+          throw new RuntimeException("No se pueden multiplicar las matrices");  
+        }else{
+            for(int i = 0;i < result.length;i++){
+                for(int j = 0; j < result[i].length;j++){
+                    for(int k = 0; k < col1;k++){
+                        result[i][j] += m1[i][k] * m2[k][j];
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 }
+
+
+
+
+
