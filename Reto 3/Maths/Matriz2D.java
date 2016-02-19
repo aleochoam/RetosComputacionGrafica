427c094d16465c2a0c4ac6f474a8db40e1942ff4
Felipe <fmontoyalvarez@gmail.com>
2016-02-18 19:04:50 -0500

    Determinante

--- Reto 3/Maths/Matriz2D.java
+++ Reto 3/Maths/Matriz2D.java
@@ -35,6 +35,35 @@ public class Matriz2D {
         return result;
     }
 
+    public static double determinant(double[][] matrix){ //method sig. takes a matrix (two dimensional array), returns determinant.
+        double sum=0; 
+        double s;
+        if(matrix.length==1){  //bottom case of recursion. size 1 matrix determinant is itself.
+            return(matrix[0][0]);
+        }
+        for(int i=0;i<matrix.length;i++){ //finds determinant using row-by-row expansion
+            double[][]smaller= new double[matrix.length-1][matrix.length-1]; //creates smaller matrix- values not in same row, column
+            for(int a=1;a<matrix.length;a++){
+                for(int b=0;b<matrix.length;b++){
+                    if(b<i){
+                        smaller[a-1][b]=matrix[a][b];
+                    }
+                    else if(b>i){
+                        smaller[a-1][b-1]=matrix[a][b];
+                    }
+                }
+            }
+            if(i%2==0){ //sign changes based on i
+                s=1;
+            }
+            else{
+                s=-1;
+            }
+            sum+=s*matrix[0][i]*(determinant(smaller)); //recursive step: determinant of larger determined by smaller.
+        }
+        return(sum); //returns determinant value. once stack is finished, returns final determinant.
+    }
+
     public String toString(){
         String res = "";
         for (int i = 0; i<matriz.length; i++) {
@@ -46,6 +75,7 @@ public class Matriz2D {
 
         return res;
     }
+    
 
 }
 
