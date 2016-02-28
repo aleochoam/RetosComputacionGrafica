5b91675c7a9f95ca36d4f5af3d41bd48f9f8750c
Alejandro <alejo8a_2@hotmail.com>
2016-02-28 12:47:55 -0500

    Cambios con errores

--- Reto 4/src/Mainclass.java
+++ Reto 4/src/Mainclass.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -10,7 +11,7 @@ import Maths.PuntoH2D;
 public class Mainclass extends JPanel{
 	
 	private Graphics2D g2d;
-	private int w = 500, h = 500;
+	private int w, h;
 	private static Chocolatina chocolatina;
 
 	public static void main(String[] args) {
@@ -23,7 +24,7 @@ public class Mainclass extends JPanel{
 			JFrame frame = new JFrame("Chocolatina");
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        frame.add(new Mainclass());
-	        frame.setSize(500, 500);
+	        frame.setSize(1000, 1000);
 	        frame.setLocationRelativeTo(null);
 	        frame.setVisible(true);
 		}
@@ -33,20 +34,19 @@ public class Mainclass extends JPanel{
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
-		
+	
+       Dimension size = getSize();
+       java.awt.Insets insets = getInsets();
+       
+       w =  size.width - insets.left - insets.right;
+       h =  size.height - insets.top - insets.bottom;
+	
 		g2d = (Graphics2D) g;
 	    g2d.setColor(Color.BLUE);
 	    g2d.drawLine(w/2, h, w/2, 0);
 	    g2d.drawLine(0, h/2, w, h/2);
 	    dibujarChocolate();
-	}
-	
-	public void waitTime(){
-		double t1 = System.currentTimeMillis();
-		while(System.currentTimeMillis() - t1 < 1000);
-	}
-	
-	
+	}	
 	
 	public void dibujarChocolate(){
 		
@@ -54,17 +54,17 @@ public class Mainclass extends JPanel{
 		int[][] aristas = Chocolatina.aristas;
 		System.err.println(aristas.length);
 		
-		for (int i = 0; i < aristas.length; i++) {
+		for (int i = 0; i < 2; i++) {
 			PuntoH2D p1 = puntos[aristas[i][0]];
 			PuntoH2D p2 = puntos[aristas[i][1]];
 			
-			System.err.println("Dibujando la linea de " + aristas[i][0] + " a "
-					+ aristas[i][1]);
+			System.err.println("Dibujando la linea de " + p1 + " a "
+					+ p2);
 			
-			g2d.drawLine((int)p1.getPunto()[0] + w/2, h/2 -(int)p1.getPunto()[1], 
+			g2d.drawLine((int)p1.getPunto()[0] + w/2,h/2 -(int)p1.getPunto()[1], 
 					(int)p2.getPunto()[0] + w/2, h/2 -(int)p1.getPunto()[1]);
 			
-			waitTime();
+			
 		}
 		
 		
