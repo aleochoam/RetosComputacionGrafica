7a3779cff7eb991995831e8fb866bba878b6b0a1
Alejandro <alejo8a_2@hotmail.com>
2016-03-04 14:29:23 -0500

    Cambios

--- Reto 5/src/Mainclass.java
+++ Reto 5/src/Mainclass.java
@@ -2,22 +2,33 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import Maths.Matriz3D;
 import Maths.PuntoH3D;
+import Trasformaciones.Perspectiva;
+import Trasformaciones.Traslacion;
 
+/**
+ * Chocolatina de tamaño 60cm * 20cm
+ * Vista desde 50 cm
+ * @author Alejandro
+ *
+ */
 public class Mainclass extends JPanel{
 	
 	private Choco3D choco;
-	
+	private int d = 10;
+
 	private Graphics2D g2d;
-	Dimension size;
+	private Dimension size;
 	private int w;
 	private int h;
-
+	
 	public static void main(String[] args) {
 		Mainclass vent = new Mainclass();
 		JFrame frame = new JFrame("Chocolatina");
@@ -50,6 +61,7 @@ public class Mainclass extends JPanel{
 	    g2d.setColor(Color.BLUE);
 	    
 	    pintarChocolate();
+	    //tomarPerspectiva();
 	}
 	
 	/**
@@ -66,6 +78,18 @@ public class Mainclass extends JPanel{
         g2d.drawLine(x1, y1, x2, y2);
     }
 	
+	public void tomarPerspectiva(double d){
+		Matriz3D m = new Perspectiva(d);
+		transform(m);
+		//repaint();
+	}
+	
+	public void rotar(){
+		Matriz3D m = new Traslacion(100,100,0);
+		transform(m);
+		
+	}
+	
 	public void transform(Matriz3D m){
 		PuntoH3D[] puntos = choco.getPuntos();
 		PuntoH3D[] nuevosPuntos = new PuntoH3D[puntos.length];
@@ -74,10 +98,14 @@ public class Mainclass extends JPanel{
 			nuevosPuntos[i] = m.tranformar(puntos[i]);
 		}
 		choco = new Choco3D(nuevosPuntos);
+		pintarChocolate();
 	}
 	
 	public void pintarChocolate(){
 		PuntoH3D[] puntos = choco.getPuntos();
+		for (int i = 0; i < puntos.length; i++) {
+			puntos[i].normalizeW();
+		}
 		int[][] aristas = Choco3D.aristas;
 		
 		for (int i = 0; i < aristas.length; i++) {
@@ -90,5 +118,18 @@ public class Mainclass extends JPanel{
 		}
 	}
 	
-	
+	class MyKeyListener implements KeyListener{
+		private double d = 0;
+		@Override
+		public void keyPressed(KeyEvent e) {}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			tomarPerspectiva(d+=10);
+			//System.out.println(e.getKeyChar());
+		}
+	}
 }
